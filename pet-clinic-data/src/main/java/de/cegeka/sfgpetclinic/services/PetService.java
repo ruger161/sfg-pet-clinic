eab508a288d644ac3ac660ba24f63594b2833a96
rjler <!cegeka1993?>
2019-07-16 11:27:25 +0200

    refacotred to common crud interface

--- pet-clinic-data/src/main/java/de/cegeka/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/de/cegeka/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package de.cegeka.sfgpetclinic.services;
 
 import de.cegeka.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/de/cegeka/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/de/cegeka/sfgpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package de.cegeka.sfgpetclinic.services;
 
 import de.cegeka.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet pet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
