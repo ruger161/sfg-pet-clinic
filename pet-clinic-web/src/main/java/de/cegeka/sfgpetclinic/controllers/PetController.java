48b0730a65d871f141342d4d5c0c0a8748098db2
rjler <!cegeka1993?>
2019-07-16 16:38:22 +0200

    Added Controller and HTML for PetVetOwner

--- pet-clinic-web/src/main/java/de/cegeka/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/de/cegeka/sfgpetclinic/controllers/PetController.java
@@ -3,12 +3,12 @@ package de.cegeka.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets"})
+@RequestMapping("/pets")
 @Controller
-public class VetController {
+public class PetController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets() {
-        return "vets/index";
+        return "pets/index";
     }
 }
